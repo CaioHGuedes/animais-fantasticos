0b7a723c6cc22f1ff93206fb6dbf670c508cafc7
Caio Guedes <caiocicm@gmail.com>
2025-03-14 16:05:33 -0300

    modal to class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -2,8 +2,9 @@ export default class TabNav {
   constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu);
     this.tabContent = document.querySelectorAll(content);
-    this.activeClass = "ativo";
+    this.activeClass = 'ativo';
   }
+
   // Ativa a tab de acordo com o index da mesma
   activeTab(index) {
     this.tabContent.forEach((section) => {
@@ -16,16 +17,15 @@ export default class TabNav {
   // Adiciona os eventos nas tabs
   addTabNavEvent() {
     this.tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => this.activeTab(index));
+      itemMenu.addEventListener('click', () => this.activeTab(index));
     });
   }
 
   init() {
     if (this.tabMenu.length && this.tabContent.length) {
-      // Ativar primeiro item
+      // ativar primeiro item
       this.activeTab(0);
       this.addTabNavEvent();
     }
-    return this;
   }
 }
